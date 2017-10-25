b085c852423873a852365cbf9357c8355e31098d
Tejas Lodaya <tejas.lodaya1@gmail.com>
2017-10-25 16:50:25 +0530

    ran formatR

--- iterators.R
+++ iterators.R
@@ -1,41 +1,42 @@
 # custom chunking iterator
 iblkrow <- function(data, chunks) {
-  
+    
     n <- nrow(data)
     i <- 1
-
+    
     nextElem <- function() {
-      
-		if (chunks <= 0 || n <= 0) stop('StopIteration')
-		m <- ceiling(n / chunks)
-		r <- seq(i, length.out=m)
-		i <<- i + m
-		n <<- n - m
-		chunks <<- chunks - 1
-
-		data[r,, drop=FALSE]
-      
+        
+        if (chunks <= 0 || n <= 0) 
+            stop("StopIteration")
+        m <- ceiling(n/chunks)
+        r <- seq(i, length.out = m)
+        i <<- i + m
+        n <<- n - m
+        chunks <<- chunks - 1
+        
+        data[r, , drop = FALSE]
+        
     }
-  
-  structure(list(nextElem=nextElem), class=c('iblkrow', 'iter'))
+    
+    structure(list(nextElem = nextElem), class = c("iblkrow", "iter"))
 }
 
 # over-riding default iterator
 nextElem.iblkrow <- function(obj) obj$nextElem()
 
-iblkgrouprow <- function(data, by, chunks){
-
-	all_combinations <- unique(data, by=by)[,..by]
-
-	# iterator inside an iterator. all_combinations will serve as data
-	itr <- iblkrow(all_combinations, chunks)
-	setkeyv(data, by)
-
-	nextElemGroup <- function(){
-		comb <- nextElem(itr)
-		data[(comb), nomatch = 0]
-	}
-	structure(list(nextElem=nextElemGroup), class=c('iblkgrouprow', 'iter'))
+iblkgrouprow <- function(data, by, chunks) {
+    
+    all_combinations <- unique(data, by = by)[, ..by]
+    
+    # iterator inside an iterator. all_combinations will serve as data
+    itr <- iblkrow(all_combinations, chunks)
+    setkeyv(data, by)
+    
+    nextElemGroup <- function() {
+        comb <- nextElem(itr)
+        data[(comb), nomatch = 0]
+    }
+    structure(list(nextElem = nextElemGroup), class = c("iblkgrouprow", "iter"))
 }
 
 # over-riding default iterator
